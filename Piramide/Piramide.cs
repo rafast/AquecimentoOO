09c96a0bcd577a055c8a3ed5b5c09c8d5638158e
Rafael Santos Telles <rafast.telles@gmail.com>
2022-05-06 08:14:36 -0300

    Corrigindo o set da propriedade N

--- Piramide/Piramide.cs
+++ Piramide/Piramide.cs
@@ -3,19 +3,29 @@ namespace AquecimentoOO
 {
     public class Piramide
     {
+        private int n;
 
-        public int N { get; set; }
+        public int N {
+            get
+            {
+                return n;
+            }
+            set
+            {
+                if(value < 1)
+                {
+                    throw new ArgumentException("O argumento N não pode ser menor que 1");
+                }
+                else
+                {
+                    n = value;
+                }
+            }
+        }
 
         public Piramide(int n)
         {
-            if(n < 1)
-            {
-                throw new ArgumentException("O argumento N não pode ser menor que 1");
-            }
-
-            this.N = n;
-
-
+            N = n;
         }
 
         public void Desenha()
@@ -23,7 +33,7 @@ namespace AquecimentoOO
             int larguraDaLinha = 0;
             int countLinha = 0;
             int countComplementoLinha = 0;
-           
+
             for (int i = 1; i <= N; i++)
             {
                 //Preenche e conta os vazios iniciais da linha
@@ -32,21 +42,21 @@ namespace AquecimentoOO
                     Console.Write(" ");
                     countLinha++;
                 }
-                
+
                 //Percorre e preenche os caracteres da linha
                 while (larguraDaLinha != 2 * i - 1)
                 {
                     //Primeira metade da piramide
-                    if (countLinha <= N -1)
+                    if (countLinha <= N - 1)
                     {
-                        Console.Write((larguraDaLinha + 1));                        
+                        Console.Write((larguraDaLinha + 1));
                         countLinha++;
                     }
                     //Segunda metade da piramide
                     else
                     {
                         countComplementoLinha++;
-                        Console.Write((larguraDaLinha + 1 - 2 * countComplementoLinha));                
+                        Console.Write((larguraDaLinha + 1 - 2 * countComplementoLinha));
                     }
 
                     larguraDaLinha++;
